1adc3462ae44947317bb13b6f4501efb79ff7e1c
Simone Celia <simone.celia@simonecelia.it>
2023-01-24 21:00:44 +0100

    reposi

---
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
