7e97f9a0dc841aa578dc3797d6bd8f1dd149f7f4
Simone Celia <simone.celia@simonecelia.it>
2023-01-26 22:08:40 +0100

    l89bis

--- fe/react-library/src/App.tsx
+++ fe/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {HomePage} from "./layouts/HomePage/HomePage";
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
 	return (
 		<div>
 			<Navbar/>
-			<HomePage />
+			{/*<HomePage/>*/}
+			<SearchBooksPage/>
 			<Footer/>
 		</div>
 	);
