884188a92185b016cc1aedb0a205c69ee4491379
simone.celia <simone.celia@eng.it>
2023-01-27 23:34:02 +0100

    l100

--- fe/react-library/src/App.tsx
+++ fe/react-library/src/App.tsx
@@ -4,13 +4,18 @@ import {HomePage} from "./layouts/HomePage/HomePage";
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
+import {Route} from "react-router";
 
 export const App = () => {
 	return (
 		<div>
 			<Navbar/>
-			{/*<HomePage/>*/}
-			<SearchBooksPage/>
+			<Route path='/'>
+				<HomePage/>
+			</Route>
+			<Route path='/search'>
+				<SearchBooksPage/>
+			</Route>
 			<Footer/>
 		</div>
 	);
