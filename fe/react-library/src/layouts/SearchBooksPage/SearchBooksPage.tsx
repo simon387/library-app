6bbc11396a4aa82ec7af3182529cb34ba36d7c7a
simone.celia <simone.celia@eng.it>
2023-01-27 22:31:17 +0100

    l96

--- fe/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ fe/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -18,7 +18,7 @@ export const SearchBooksPage = () => {
 	useEffect(() => {
 		const fetchBooks = async () => {
 			const baseUrl: string = "http://localhost:8080/api/books";
-			let url: string = '';
+			let url: string;
 
 			if (searchUrl === '') {
 				url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
@@ -137,15 +137,26 @@ export const SearchBooksPage = () => {
 							</div>
 						</div>
 					</div>
-					<div className='mt-3'>
-						<h5>Number of result: ({totalAmountOfBooks})</h5>
-					</div>
-					<p>
-						{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-					</p>
-					{books.map(book => (
-						<SearchBook book={book} key={book.id}/>
-					))}
+					{totalAmountOfBooks > 0 ?
+						<>
+							<div className='mt-3'>
+								<h5>Number of result: ({totalAmountOfBooks})</h5>
+							</div>
+							<p>
+								{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+							</p>
+							{books.map(book => (
+								<SearchBook book={book} key={book.id}/>
+							))}
+						</>
+						:
+						<div className='m-5'>
+							<h3>
+								Can't find what you are looking for?
+							</h3>
+							<a type='button' className='btn main-color btn-,d px-4 me-md-2 fw-bold text-white' href='#'>Library Services</a>
+						</div>
+					}
 					{totalPages > 1 &&
 						<Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>
 					}
