07a1d157a332b905cb0ef3658d53f2b3bfea3da3
Simone Celia <simone.celia@simonecelia.it>
2023-01-28 22:33:54 +0100

    l111

--- fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
+++ fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
@@ -69,7 +69,7 @@ export const BookCheckOutPage = () => {
 							<h2>{book?.title}</h2>
 							<h5 className='text-primary'>{book?.author}</h5>
 							<p className='lead'>{book?.description}</p>
-							<StarsReview rating={4} size={32}/>
+							<StarsReview rating={4.5} size={32}/>
 						</div>
 					</div>
 				</div>
@@ -88,7 +88,7 @@ export const BookCheckOutPage = () => {
 						<h2>{book?.title}</h2>
 						<h5 className='text-primary'>{book?.author}</h5>
 						<p className='lead'>{book?.description}</p>
-						<StarsReview rating={4} size={32}/>
+						<StarsReview rating={4.5} size={32}/>
 					</div>
 				</div>
 				<hr/>
