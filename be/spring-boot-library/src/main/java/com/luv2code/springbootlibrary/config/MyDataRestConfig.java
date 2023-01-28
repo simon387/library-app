4eff4ddf34d5ce5b2d4bafb3e0c7fad42386d4f3
simone.celia <simone.celia@eng.it>
2023-01-28 23:27:19 +0100

    l115

--- be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.luv2code.springbootlibrary.config;
 
 import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,9 +13,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 	@Override public void configureRepositoryRestConfiguration ( RepositoryRestConfiguration config, CorsRegistry cors ) {
 		HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT };
+
 		config.exposeIdsFor ( Book.class );
+		config.exposeIdsFor ( Review.class );
 
 		disableHttpMethods ( Book.class, config, theUnsupportedActions );
+		disableHttpMethods ( Review.class, config, theUnsupportedActions );
 
 		/* Configure CORS Mapping */
 		String theAllowerOrigins = "http://localhost:3000";
