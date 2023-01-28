df4815cb3589aabe1b63169714288aeb437dae56
Simone Celia <simone.celia@simonecelia.it>
2023-01-28 16:49:36 +0100

    L107

--- fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
+++ fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
@@ -52,8 +52,43 @@ export const BookCheckOutPage = () => {
 	}
 
 	return (
-		<div className=''>
-			<h3> </h3>
+		<div>
+			<div className='container d-none d-lg-block'>
+				<div className='row mt-5'>
+					<div className='col-sm-2 col-md-2'>
+						{book?.img ?
+							<img src={book?.img} width='226' height='349' alt='book' />
+							:
+							<img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book'/>
+						}
+					</div>
+					<div className='col-4 col-md-4 container'>
+						<div className='ml-2'>
+							<h2>{book?.title}</h2>
+							<h5 className='text-primary'>{book?.author}</h5>
+							<p className='lead'>{book?.description}</p>
+						</div>
+					</div>
+				</div>
+				<hr/>
+			</div>
+			<div className='container d-lg-none mt-5'>
+				<div className='d-flex justify-content-center align-item-center'>
+					{book?.img ?
+						<img src={book?.img} width='226' height='349' alt='book' />
+						:
+						<img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226' height='349' alt='Book'/>
+					}
+				</div>
+				<div className='mt-4'>
+					<div className='ml-2'>
+						<h2>{book?.title}</h2>
+						<h5 className='text-primary'>{book?.author}</h5>
+						<p className='lead'>{book?.description}</p>
+					</div>
+				</div>
+				<hr/>
+			</div>
 		</div>
 	);
 }
