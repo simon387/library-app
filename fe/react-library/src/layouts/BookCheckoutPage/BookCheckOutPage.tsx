dd51f265883ad4bf2c0c9cb67276245a597b29bc
simone.celia <simone.celia@eng.it>
2023-01-28 16:39:05 +0100

    L106

--- fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
+++ fe/react-library/src/layouts/BookCheckoutPage/BookCheckOutPage.tsx
@@ -1,16 +1,59 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import BookModel from "../../models/BookModel";
+import {SpinnerLoading} from "../Utils/SpinnerLoading";
 
 export const BookCheckOutPage = () => {
 	const [book, setBook] = useState<BookModel>();
-	const [isLoadingBook, setIsLoadingBook] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 	const [httpError, setHttpError] = useState(null);
 
 	const bookId = (window.location.pathname).split('/')[2];
 
+	useEffect(() => {
+		const fetchBook = async () => {
+			const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+			const response = await fetch(baseUrl);
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+			const responseJson = await response.json();
+			const loadedBook: BookModel = {
+				id: responseJson.id,
+				title: responseJson.author,
+				description: responseJson.description,
+				copies: responseJson.copies,
+				copiesAvailable: responseJson.copiesAvailable,
+				category: responseJson.category,
+				img: responseJson.img,
+			};
+			setBook(loadedBook);
+			setIsLoading(false);
+		};
+		fetchBook().catch((error: any) => {
+			setIsLoading(false);
+			setHttpError(error.messsage);
+		});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div className='container m-5'>
+				<SpinnerLoading/>
+			</div>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<div className='container m-5'>
+				<p>{httpError}</p>
+			</div>
+		);
+	}
+
 	return (
 		<div className=''>
-			<h3>Hi World!</h3>
+			<h3> </h3>
 		</div>
 	);
 }
