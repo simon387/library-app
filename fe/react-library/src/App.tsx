293ecd962dc0d40dd40a0d83a2271e4839911ffa
Simone Celia <simone.celia@simonecelia.it>
2023-01-30 18:16:04 +0100

    130

--- fe/react-library/src/App.tsx
+++ fe/react-library/src/App.tsx
@@ -4,10 +4,24 @@ import {HomePage} from "./layouts/HomePage/HomePage";
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
-import {Redirect, Route, Switch} from "react-router";
+import {Redirect, Route, Switch, useHistory} from "react-router";
 import {BookCheckOutPage} from "./layouts/BookCheckoutPage/BookCheckOutPage";
+import {oktaConfig} from "./lib/oktaConfig";
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+	const customAuthHandler = () => {
+		history.push('/');
+	}
+
+	const history = useHistory();
+
+	const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+		history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+	}
+
 	return (
 		<div className='d-flex flex-column min-vh-100'>
 			<Navbar/>
@@ -22,9 +36,9 @@ export const App = () => {
 					<Route path='/search'>
 						<SearchBooksPage/>
 					</Route>
-				<Route path='/checkout/:bookId'>
-					<BookCheckOutPage />
-				</Route>
+					<Route path='/checkout/:bookId'>
+						<BookCheckOutPage/>
+					</Route>
 				</Switch>
 			</div>
 			<Footer/>
