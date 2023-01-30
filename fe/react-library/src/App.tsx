75a5df90ae5b87a6e0bdbc78f5350fbfb9eb5739
Simone Celia <simone.celia@simonecelia.it>
2023-01-30 18:34:20 +0100

    fix_client_id

--- fe/react-library/src/App.tsx
+++ fe/react-library/src/App.tsx
@@ -8,40 +8,49 @@ import {Redirect, Route, Switch, useHistory} from "react-router";
 import {BookCheckOutPage} from "./layouts/BookCheckoutPage/BookCheckOutPage";
 import {oktaConfig} from "./lib/oktaConfig";
 import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+import {LoginCallback, Security} from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
 	const customAuthHandler = () => {
-		history.push('/');
+		history.push('/login');
 	}
 
 	const history = useHistory();
 
 	const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
 		history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
-	}
+	};
 
 	return (
 		<div className='d-flex flex-column min-vh-100'>
-			<Navbar/>
-			<div className='flex-grow-1'>
-				<Switch>
-					<Route path='/' exact>
-						<Redirect to={'/home'}/>
-					</Route>
-					<Route path='/home'>
-						<HomePage/>
-					</Route>
-					<Route path='/search'>
-						<SearchBooksPage/>
-					</Route>
-					<Route path='/checkout/:bookId'>
-						<BookCheckOutPage/>
-					</Route>
-				</Switch>
-			</div>
-			<Footer/>
+			<Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+				<Navbar/>
+				<div className='flex-grow-1'>
+					<Switch>
+						<Route path='/' exact>
+							<Redirect to='/home'/>
+						</Route>
+						<Route path='/home'>
+							<HomePage/>
+						</Route>
+						<Route path='/search'>
+							<SearchBooksPage/>
+						</Route>
+						<Route path='/checkout/:bookId'>
+							<BookCheckOutPage/>
+						</Route>
+						<Route path='/login' render={
+							() => <LoginWidget config={oktaConfig}/>
+						}
+						/>
+						<Route path='/login/callback' component={LoginCallback}/>
+					</Switch>
+				</div>
+				<Footer/>
+			</Security>
 		</div>
 	);
 }
--- fe/react-library/src/Auth/OktaSignInWidget.jsx
+++ fe/react-library/src/Auth/OktaSignInWidget.jsx
@@ -1,7 +1,7 @@
-import {useEffect, useRef} from "react";
-import OktaSignIn from "@okta/okta-signin-widget";
+import {useEffect, useRef} from 'react';
+import OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
-import {oktaConfig} from "../lib/oktaConfig";
+import {oktaConfig} from '../lib/oktaConfig';
 
 const OktaSignInWidget = ({onSuccess, onError}) => {
 	const widgetRef = useRef();
@@ -17,7 +17,7 @@ const OktaSignInWidget = ({onSuccess, onError}) => {
 			el: widgetRef.current,
 		}).then(onSuccess).catch(onError);
 
-		return () => widget.remove;
+		return () => widget.remove();
 	}, [onSuccess, onError]);
 
 	return (
@@ -25,6 +25,6 @@ const OktaSignInWidget = ({onSuccess, onError}) => {
 			<div ref={widgetRef}></div>
 		</div>
 	);
-}
+};
 
 export default OktaSignInWidget;
