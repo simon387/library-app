28004383935a47453dd8f4ef4d1b16b48f796a8d
Simone Celia <simone.celia@simonecelia.it>
2023-01-30 23:07:21 +0100

    l139

--- be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -4,6 +4,7 @@ import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,12 @@ public class BookController {
 	@Autowired
 	public BookController ( BookService bookService ) {
 		this.bookService = bookService;
+	}
+
+	@GetMapping ( "/secure/ischeckedout/byuser" )
+	public Boolean checkoutBookByUser ( @RequestParam Long bookId ) {
+		String userEmail = "testuser@email.com";
+		return bookService.checkoutBookByUser ( userEmail, bookId );
 	}
 
 	@PutMapping ( "/secure/checkout" )
