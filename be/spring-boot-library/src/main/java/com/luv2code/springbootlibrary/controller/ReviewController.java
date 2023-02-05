5f0c5aa8c6ec0b9a7cd154ff9f80cbaf9c5e92ce
Simone Celia <simone.celia@simonecelia.it>
2023-02-05 23:39:32 +0100

    l158

---
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.requestmodels.ReviewRequest;
+import com.luv2code.springbootlibrary.service.ReviewService;
+import com.luv2code.springbootlibrary.util.ExtractJWT;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin ( "http://localhost:3000" )
+@RestController
+@RequestMapping ( "/api/reviews" )
+public class ReviewController {
+
+	private final ReviewService reviewService;
+
+	public ReviewController ( ReviewService reviewService ) {
+		this.reviewService = reviewService;
+	}
+
+	@PostMapping ( "/secure" )
+	public void postReview ( @RequestHeader ( value = "Authorization" ) String token, @RequestBody ReviewRequest reviewRequest ) throws Exception {
+		String userEmail = ExtractJWT.payloadJWTExtraction ( token, "\"sub\"" );
+		if ( userEmail == null ) {
+			throw new Exception ( "User email is missing" );
+		}
+		reviewService.postReview ( userEmail, reviewRequest );
+	}
+}
