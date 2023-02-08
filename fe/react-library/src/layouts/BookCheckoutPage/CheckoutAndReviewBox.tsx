bbec931ed5461f790b0d0f86bc621f39ce594b8e
Simone Celia <simone.celia@simonecelia.it>
2023-02-08 02:25:25 +0100

    l162

--- fe/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ fe/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 export const CheckoutAndReviewBox: React.FC<{
 	book: BookModel | undefined, mobile: boolean,
-	currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any
+	currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any, isReviewLeft: boolean
 }> = (props) => {
 
 	function buttonRender() {
@@ -25,6 +25,24 @@ export const CheckoutAndReviewBox: React.FC<{
 		}
 		return (
 			<Link to={'/login'} className='btn btn-success btn-lg'>Sign in</Link>
+		);
+	}
+
+	function reviewRender() {
+		if (props.isAuthenticated && !props.isReviewLeft) {
+			return(
+				<p>Leave a review component here.</p>
+			);
+		} else if (props.isAuthenticated && props.isReviewLeft) {
+			return (
+				<p><b>Thank you for your review!</b></p>
+			);
+		}
+		return (
+			<div>
+				<hr/>
+				<p>Sign in to be able to leave a review.</p>
+			</div>
 		);
 	}
 
@@ -62,9 +80,7 @@ export const CheckoutAndReviewBox: React.FC<{
 				<p className='mt-3'>
 					This number can change until placing order has been complete.
 				</p>
-				<p>
-					Sign in to be able to leave a review.
-				</p>
+				{reviewRender()}
 			</div>
 		</div>
 	);
