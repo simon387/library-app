b77c1090f31ff3a54530bdc7eae1512c00cd5d30
simone.celia <simone.celia@eng.it>
2023-02-08 02:25:25 +0100

    l162

--- fe/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ fe/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -236,6 +236,7 @@ export const BookCheckoutPage = () => {
 						isAuthenticated={authState?.isAuthenticated}
 						isCheckedOut={isCheckedOut}
 						checkoutBook={checkoutBook}
+						isReviewLeft={isReviewLeft}
 					/>
 				</div>
 				<hr/>
@@ -264,6 +265,7 @@ export const BookCheckoutPage = () => {
 					isAuthenticated={authState?.isAuthenticated}
 					isCheckedOut={isCheckedOut}
 					checkoutBook={checkoutBook}
+					isReviewLeft={isReviewLeft}
 				/>
 				<hr/>
 				<LatestReviews reviews={reviews} bookId={book?.id} mobile={true}/>
