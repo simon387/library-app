2b3f4972be7d88fb5b8e73dbe3e517ee33daf123
simone.celia <simone.celia@eng.it>
2023-02-09 17:25:07 +0100

    l180

--- be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -18,5 +18,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 	Page<Book> findByCategory ( @RequestParam ( "category" ) String category, Pageable pageable );
 
 	@Query("select o from Book o where o.id in :book_ids")
-	List<Book> findBooksByBooksIds ( @Param ( "book_ids" ) List<Long> bookId );
+	List<Book> findBooksByBookIds ( @Param ( "book_ids" ) List<Long> bookId );
 }
--- be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -5,13 +5,18 @@ import com.luv2code.springbootlibrary.dao.CheckoutRepository;
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.entity.Checkout;
 import com.luv2code.springbootlibrary.responemodels.ShelfCurrentLoansResponse;
+import org.hibernate.annotations.Check;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -70,7 +75,26 @@ public class BookService {
 			bookIdList.add ( i.getBookId () );
 		}
 
-		List<Book> books = bookRepository.findBooksByBookIds(bookIdList);
+		List<Book> books = bookRepository.findBooksByBookIds ( bookIdList );
 
+		SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd" );
+
+		for ( Book book : books ) {
+			Optional<Checkout> checkout = checkoutList.stream ()
+							.filter ( x -> Objects.equals ( x.getBookId (), book.getId () ) ).findFirst ();
+
+			if ( checkout.isPresent () ) {
+
+				Date d1 = sdf.parse ( checkout.get ().getReturnDate () );
+				Date d2 = sdf.parse ( LocalDate.now ().toString () );
+
+				TimeUnit time = TimeUnit.DAYS;
+
+				long difference_In_Time = time.convert ( d1.getTime () - d2.getTime (), TimeUnit.MILLISECONDS );
+
+				shelfCurrentLoansResponses.add ( new ShelfCurrentLoansResponse ( book, (int) difference_In_Time ) );
+			}
+		}
+		return shelfCurrentLoansResponses;
 	}
 }
