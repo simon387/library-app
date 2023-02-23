f33e4729ce7e5b3f770473f63da274fc33ae3c04
simone.celia <simone.celia@eng.it>
2023-02-23 20:09:54 +0100

    repository_message

---
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}
