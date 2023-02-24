eb36d7256db1d20192ec7ae6247315e5cdd2e343
simone.celia <simone.celia@eng.it>
2023-02-24 22:38:07 +0100

    new_comè

--- fe/react-library/src/layouts/MessagesPage/MessagesPage.tsx
+++ fe/react-library/src/layouts/MessagesPage/MessagesPage.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {PostNewMessage} from "./components/PostNewMessage";
 
 export const MessagesPage = () => {
 
@@ -21,7 +22,7 @@ export const MessagesPage = () => {
 				</nav>
 				<div className='tab-content' id='nav-tabContent'>
 					<div className='tab-pane fade show active' id='nav-send-message' role='tabpanel' aria-labelledby='nav-send-message-tab'>
-						<p>Post new message</p>
+						<PostNewMessage/>
 					</div>
 					<div className='tab-pane fade' id='nav-message' role='tabpanel' aria-labelledby='nav-message-tab'>
 						{messagesClick ? <p>Messages</p> : <></>}
