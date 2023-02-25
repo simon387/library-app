792f1da37fe6fb2276f7b27c4f1709849792bfed
simone.celia <simone.celia@eng.it>
2023-02-25 19:44:07 +0100

    213

--- fe/react-library/src/layouts/MessagesPage/components/PostNewMessage.tsx
+++ fe/react-library/src/layouts/MessagesPage/components/PostNewMessage.tsx
@@ -1,5 +1,6 @@
 import {useOktaAuth} from "@okta/okta-react";
 import {useState} from "react";
+import MessageModel from "../../../models/MessageModel";
 
 export const PostNewMessage = () => {
 
@@ -9,13 +10,36 @@ export const PostNewMessage = () => {
 	const [displayWarning, setDisplayWarning] = useState(false);
 	const [displaySuccess, setDisplaySuccess] = useState(false);
 
+	async function submitNewQuestion() {
+		const url = `http://localhost:8080/api/messages/secure/add/message`;
+		if (authState?.isAuthenticated && title !== '' && question !== '') {
+			const messageRequestModel: MessageModel = new MessageModel(title, question);
+			const requestOptions = {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(messageRequestModel)
+			};
+
+			const submitNewQuestionResponse = await fetch(url, requestOptions);
+			if (!submitNewQuestionResponse.ok) {
+				throw new Error('Something went wrong!');
+			}
+
+			setTitle('');
+			setQuestion('');
+			setDisplayWarning(false);
+			setDisplaySuccess(true);
+		} else {
+			setDisplayWarning(true);
+			setDisplaySuccess(false);
+		}
+	}
+
 	return (
 		<div className='card mt-3'>
-			{displaySuccess &&
-				<div className='aler alert-success' role='alert'>
-					Question added successfully
-				</div>
-			}
 			<div className='card-header'>
 				Ask question to Luv 2 Read Admin
 			</div>
@@ -24,6 +48,11 @@ export const PostNewMessage = () => {
 					{displayWarning &&
 						<div className='alert alert-danger' role='alert'>
 							All fields must be filled out
+						</div>
+					}
+					{displaySuccess &&
+						<div className='alert alert-success' role='alert'>
+							Question added successfully
 						</div>
 					}
 					<div className='mb-3'>
@@ -41,7 +70,7 @@ export const PostNewMessage = () => {
 						<textarea className='form-control' id='exampleFormControlTextarea1' rows={3} onChange={e => setQuestion(e.target.value)} value={question}/>
 					</div>
 					<div>
-						<button type='button' className='btn btn-primary mt-3'>
+						<button type='button' className='btn btn-primary mt-3' onClick={submitNewQuestion}>
 							Submit Question
 						</button>
 					</div>
