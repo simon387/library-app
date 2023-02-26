fdcb056539f0d60258ec473ba99cad0780bebfdf
Simone Celia <simone.celia@simonecelia.it>
2023-02-26 21:48:48 +0100

    L-235

--- fe/react-library/src/layouts/ManageLibraryPage/components/AdminMessage.tsx
+++ fe/react-library/src/layouts/ManageLibraryPage/components/AdminMessage.tsx
@@ -1,12 +1,19 @@
 import MessageModel from "../../../models/MessageModel";
 import {useState} from "react";
 
-export const AdminMessage: React.FC<{ message: MessageModel }> = (props, key) => {
+export const AdminMessage: React.FC<{ message: MessageModel, submitResponseToQuestion: any }> = (props, key) => {
 
 	const [displayWarning, setDisplayWarning] = useState(false);
 	const [response, setResponse] = useState("");
 
-
+	function submitBtn() {
+		if (props.message.id !== null && response !== '') {
+			props.submitResponseToQuestion(props.message.id, response);
+			setDisplayWarning(false);
+		} else {
+			setDisplayWarning(true);
+		}
+	}
 
 	return (
 		<div key={props.message.id}>
@@ -27,10 +34,11 @@ export const AdminMessage: React.FC<{ message: MessageModel }> = (props, key) =>
 							<label className='form-label'>
 								Description
 							</label>
-							<textarea className='form-control' id='exampleFormControlTextarea1' rows={3} onChange={e => setResponse(e.target.value)} value={response}></textarea>
+							<textarea className='form-control' id='exampleFormControlTextarea1' rows={3} onChange={e => setResponse(e.target.value)}
+												value={response}></textarea>
 						</div>
 						<div>
-							<button type='button' className='btn btn-primary mt-3'>
+							<button type='button' className='btn btn-primary mt-3' onClick={submitBtn}>
 								Submit Response
 							</button>
 						</div>
