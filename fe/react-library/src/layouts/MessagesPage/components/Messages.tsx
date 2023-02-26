1889b08b1dd39f706c621d883c60b3612775980f
Simone Celia <simone.celia@simonecelia.it>
2023-02-26 18:08:02 +0100

    L-217

--- fe/react-library/src/layouts/MessagesPage/components/Messages.tsx
+++ fe/react-library/src/layouts/MessagesPage/components/Messages.tsx
@@ -2,6 +2,7 @@ import {useOktaAuth} from "@okta/okta-react";
 import {useEffect, useState} from "react";
 import MessageModel from "../../../models/MessageModel";
 import {SpinnerLoading} from "../../Utils/SpinnerLoading";
+import {Pagination} from "../../Utils/Pagination";
 
 export const Messages = () => {
 
@@ -25,7 +26,7 @@ export const Messages = () => {
 					method: 'GET',
 					headers: {
 						Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
-						'Content-Type:': 'application/json'
+						'Content-Type': 'application/json'
 					}
 				};
 				const messagesResponse = await fetch(url, requestOptions);
@@ -62,7 +63,36 @@ export const Messages = () => {
 	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	return (
-		<>
-		</>
+		<div className='mt-2'>
+			{messages.length > 0 ?
+				<>
+					<h5>Current Q/A: </h5>
+					{messages.map(message => (
+						<div key={message.id}>
+							<div className='card mt-2 shadow p-3 bg-body rounded'>
+								<h5>Case #{message.id}: {message.title}</h5>
+								<h6>{message.userEmail}</h6>
+								<p>{message.question}</p>
+								<hr/>
+								<div>
+									<h5>Response: </h5>
+									{message.response && message.adminEmail ?
+										<>
+											<h6>{message.adminEmail} (admin)</h6>
+											<p>{message.response}</p>
+										</>
+										:
+										<p><i>Pending response from administration. Please be patient.</i></p>
+									}
+								</div>
+							</div>
+						</div>
+					))}
+				</>
+				:
+				<h5>All questions you submit will be shown here</h5>
+			}
+			{totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
+		</div>
 	);
 }
