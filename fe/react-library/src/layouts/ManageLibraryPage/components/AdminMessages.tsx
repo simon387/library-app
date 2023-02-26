2ef332c515ac6272f05bff7d2dbbd22e47b4c577
Simone Celia <simone.celia@simonecelia.it>
2023-02-26 21:28:36 +0100

    L-229

--- fe/react-library/src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ fe/react-library/src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import MessageModel from "../../../models/MessageModel";
 import {SpinnerLoading} from "../../Utils/SpinnerLoading";
 import {Pagination} from "../../Utils/Pagination";
+import {AdminMessage} from "./AdminMessage";
 
 export const AdminMessages = () => {
 
@@ -71,7 +72,7 @@ export const AdminMessages = () => {
 				<>
 					<h5>Pending Q/A: </h5>
 					{messages.map(message => (
-						<p>Questions that need a response</p>
+						<AdminMessage message={message} key={message.id}/>
 					))}
 				</>
 				:
