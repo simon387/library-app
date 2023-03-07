ce86aa92777d2ea925b6ec39e3d65704b482f456
Simone Celia <simone.celia@simonecelia.it>
2023-03-07 23:14:41 +0100

    l-247

---
+++ fe/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -0,0 +1,21 @@
+import BookModel from "../../../models/BookModel";
+import {useEffect, useState} from "react";
+
+export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) => {
+
+	const [quantity, setQuantity] = useState<number>(0);
+	const [remaining, setRemaining] = useState<number>(0);
+
+	useEffect(() => {
+		const fetchBookInState = () => {
+			props.book.copies ? setQuantity(props.book.copies) : setQuantity(0);
+			props.book.copiesAvailable ? setRemaining(props.book.copiesAvailable) : setRemaining(0);
+		};
+		fetchBookInState();
+	}, []);
+
+	return (
+		<>
+		</>
+	);
+}
