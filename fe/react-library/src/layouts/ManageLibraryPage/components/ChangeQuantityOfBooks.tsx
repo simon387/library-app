e3f5e56b21bdc88f61f09c74760cf437e630efbf
simone.celia <simone.celia@eng.it>
2023-03-07 23:49:06 +0100

    l-248

--- fe/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
+++ fe/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import BookModel from "../../../models/BookModel";
 import {SpinnerLoading} from "../../Utils/SpinnerLoading";
 import {Pagination} from "../../Utils/Pagination";
+import {ChangeQuantityOfBook} from "./ChangeQuantityOfBook";
 
 export const ChangeQuantityOfBooks = () => {
 
@@ -83,7 +84,7 @@ export const ChangeQuantityOfBooks = () => {
 						{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
 					</p>
 					{books.map(book => (
-						<p>Display different quantity of books</p>
+						<ChangeQuantityOfBook book={book} key={book.id}/>
 					))
 					}
 				</>
