af6102cd423b4e5c49f94016a946316a38277b19
simone.celia <simone.celia@eng.it>
2023-03-11 00:26:18 +0100

    l-280

---
+++ be/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/requestmodels/PaymentInfoRequest.java
@@ -0,0 +1,14 @@
+package com.luv2code.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+
+@Data
+public class PaymentInfoRequest {
+
+	private int amount;
+
+	private String currency;
+
+	private String receiptEmail;
+}
