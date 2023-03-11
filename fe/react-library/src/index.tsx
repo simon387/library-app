94444c5c503fb2b407224294107a12636be6601a
simone.celia <simone.celia@eng.it>
2023-03-11 20:18:30 +0100

    l-290

--- fe/react-library/src/index.tsx
+++ fe/react-library/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_live_51IuKnqCW5yRxefpVEKqxOylcqVCmpOw9KjRDXXJe8XTFQmocm1DZnQbdCbELkKW80ZIXYdQDA8n6jCYncFYAZPkB00DbFcs8Se');
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<BrowserRouter>
-		<App/>
+		<Elements stripe={stripePromise}>
+			<App/>
+		</Elements>
 	</BrowserRouter>
 );
