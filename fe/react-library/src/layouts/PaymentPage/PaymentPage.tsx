723a5883eb0136ede15098932156f7790618450c
simone.celia <simone.celia@eng.it>
2023-03-11 20:50:46 +0100

    l-294

--- fe/react-library/src/layouts/PaymentPage/PaymentPage.tsx
+++ fe/react-library/src/layouts/PaymentPage/PaymentPage.tsx
@@ -1,5 +1,5 @@
 import {useOktaAuth} from "@okta/okta-react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {SpinnerLoading} from "../Utils/SpinnerLoading";
 
 export const PaymentPage = () => {
@@ -9,6 +9,32 @@ export const PaymentPage = () => {
 	const [submitDisabled, setSubmitDisabled] = useState(false);
 	const [fees, setFees] = useState(0);
 	const [loadingFees, setLoadingFees] = useState(true);
+
+	useEffect(() => {
+		const fetchFees = async () => {
+			if (authState && authState.isAuthenticated) {
+				const url = `${process.env.REACT_APP_API}/payments/search/findByUserEmail?userEmail=${authState.accessToken?.claims.sub}`;
+				const requestOptions = {
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+						'Content-Type': 'application/json'
+					}
+				};
+				const paymentResponse = await fetch(url, requestOptions);
+				if (!paymentResponse.ok) {
+					throw new Error('Something went wrong!');
+				}
+				const paymentResponseJson = await paymentResponse.json();
+				setFees(paymentResponseJson.amount);
+				setLoadingFees(false);
+			}
+		}
+		fetchFees().catch((error: any) => {
+			setLoadingFees(false);
+			setHttpError(error.message);
+		});
+	}, [authState]);
 
 	if (loadingFees) {
 		return (
