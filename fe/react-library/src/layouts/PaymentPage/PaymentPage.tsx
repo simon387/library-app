0f708d0dfcc341d34ec1a051369ff04db1075976
simone.celia <simone.celia@eng.it>
2023-03-11 20:43:16 +0100

    l-294

---
+++ fe/react-library/src/layouts/PaymentPage/PaymentPage.tsx
@@ -0,0 +1,31 @@
+import {useOktaAuth} from "@okta/okta-react";
+import {useState} from "react";
+import {SpinnerLoading} from "../Utils/SpinnerLoading";
+
+export const PaymentPage = () => {
+
+	const {authState} = useOktaAuth();
+	const [httpError, setHttpError] = useState(false);
+	const [submitDisabled, setSubmitDisabled] = useState(false);
+	const [fees, setFees] = useState(0);
+	const [loadingFees, setLoadingFees] = useState(true);
+
+	if (loadingFees) {
+		return (
+			<SpinnerLoading/>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<div className='container m-5'>
+				<p>{httpError}</p>
+			</div>
+		);
+	}
+
+	return (
+		<>
+		</>
+	);
+}
